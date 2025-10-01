962d6d7a7f16fd07e8e45a29cbd32d854a852cd2
Kirti Phegade <kirtiphegade14@gmail.com>
2025-10-01 13:58:32 +0530

    UI Implement

---
+++ FileCurd/src/main/java/com/file_oprations/FileCurd/FileCurdApplication.java
@@ -0,0 +1,13 @@
+package com.file_oprations.FileCurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileCurdApplication.class, args);
+	}
+
+}
---
+++ FileCurd/src/test/java/com/file_oprations/FileCurd/FileCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.file_oprations.FileCurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
